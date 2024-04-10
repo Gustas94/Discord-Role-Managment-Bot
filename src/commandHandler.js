5e166cfd3c8516f418904262c67180a9bdf4bccf
BuildTools <unconfigured@null.spigotmc.org>
2024-04-10 07:05:58 +0300

    Removed removeAllRolesCommand, because it interfiered with how the bot works

--- src/commandHandler.js
+++ src/commandHandler.js
@@ -1,6 +1,5 @@
 // commandHandler.js
 const addRoleCommand = require('./commands/addRoleCommand');
-const removeAllRolesCommand = require('./commands/removeAllRolesCommand'); // Import the new command
 
 module.exports = (client) => {
     client.on('interactionCreate', async interaction => {
@@ -9,9 +8,6 @@ module.exports = (client) => {
         switch (interaction.commandName) {
             case 'addrole':
                 await addRoleCommand.handle(interaction);
-                break;
-            case 'removeallroles':
-                await removeAllRolesCommand.handle(interaction);
                 break;
         }
     });
--- src/commands.js
+++ src/commands.js
@@ -8,9 +8,4 @@ const addRoleCommand = new SlashCommandBuilder()
     .addStringOption(option => option.setName('rolename').setDescription('The name of the role').setRequired(true))
     .addStringOption(option => option.setName('dependencies').setDescription('Space-separated list of dependency role IDs').setRequired(true));
 
-const removeAllRolesCommand = new SlashCommandBuilder()
-    .setName('removeallroles')
-    .setDescription('Remove a specific role from all users')
-    .addStringOption(option => option.setName('roleid').setDescription('The ID of the role to remove from all users').setRequired(true));
-
-module.exports = [addRoleCommand, removeAllRolesCommand]; 
+module.exports = [addRoleCommand]; 
